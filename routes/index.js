b96b4269a898380b2f1946b560c825f2a9c1b992
Akira F <akiraff@hotmail.com>
2018-06-01 11:36:18 +1000

    implemented few things

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', (req, res) => res.render('index', { title: 'Express', page_name: 'index' }));
+router.get('/work', (req, res) => res.render('about', {title: 'field | about', page_name: 'about'}));
+router.get('/enquiries', (req, res) => res.render('enquiries', {title: 'field | enquiries', page_name: 'enquiries'}));
+router.get('/workforus', (req, res) => res.render('workforus', {title: 'field | work(s) for us', page_name: 'work'}));
 
 module.exports = router;
